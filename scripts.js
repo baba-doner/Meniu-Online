9e5450274fd36ce79d01e0dc9a911925035e1ccd
baba-doner <135450542+baba-doner@users.noreply.github.com>
2023-06-22 20:28:31 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,9 +17,10 @@ function incarcaMeniu()
                 var cardProdus='<div class="cardProdus"><b>'+ produs.numeProdus + '</b> ('+produs.gramajProdus+') <p style="color:green;display:inline;position: absolute; right: 2rem;font-size:1rem;">'+produs.pretProdus+' lei</p>';
                 cardProdus+='<br/><div style="color:gray;">'+produs.descriereProdus+'</div>';
                 cardProdus+='<div style="font-size:12px;padding:1.2rem">'
-                cardProdus+='<br/><p><b>Informații nutriționale 100g:</b></p><div style="color:gray;">'+ produs.nutritieProdus+'</div>';
+                
                 if(produs.alergeniProdus!==''&& produs.alergeniProdus!=='-')
                 {
+cardProdus+='<br/><p><b>Informații nutriționale 100g:</b></p><div style="color:gray;">'+ produs.nutritieProdus+'</div>';
                     cardProdus+='<p><b>Alergeni:</b></p><div style="color:gray;">'+ produs.alergeniProdus+'</div>';
                 }
                 
@@ -34,9 +35,10 @@ function incarcaMeniu()
                 var cardProdus='<div class="cardProdus"><b>'+ produs.numeProdus + '</b> ('+produs.gramajProdus+') <p style="color:green;display:inline;position: absolute; right: 2rem;font-size:1rem;">'+produs.pretProdus+' lei</p>';
                 cardProdus+='<br/><div style="color:gray;">'+produs.descriereProdus+'</div>'
                 cardProdus+='<div style="font-size:12px;padding-left:1.2rem">';
-                cardProdus+='<br/><p><b>Informații nutriționale 100g:</b></p><div style="color:gray;">'+ produs.nutritieProdus+'</div>';
+                
                 if(produs.alergeniProdus!=='' && produs.alergeniProdus!=='-')
                 {
+cardProdus+='<br/><p><b>Informații nutriționale 100g:</b></p><div style="color:gray;">'+ produs.nutritieProdus+'</div>';
                     cardProdus+='<p><b>Alergeni:</b></p><div style="color:gray;">'+ produs.alergeniProdus+'</div>';
                 }
                 cardProdus+='</div></div>';
