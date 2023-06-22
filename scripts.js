f5a9f313e49bd1646d5e3cc74a0332bec1899873
baba-doner <135450542+baba-doner@users.noreply.github.com>
2023-06-22 20:21:42 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ function incarcaMeniu()
             if(categ===categCurenta)
             {
                 var cardProdus='<div class="cardProdus"><b>'+ produs.numeProdus + '</b> ('+produs.gramajProdus+') <p style="color:green;display:inline;position: absolute; right: 2rem;font-size:1rem;">'+produs.pretProdus+' lei</p>';
-                cardProdus+='<br/><div style="color:gray;">'+produs.descriereProdus+'</div>'
+                cardProdus+='<br/><div style="color:gray;">'+produs.descriereProdus+'</div>';
                 cardProdus+='<div style="font-size:12px;padding:1.2rem">'
                 cardProdus+='<br/><p><b>Informații nutriționale 100g:</b></p><div style="color:gray;">'+ produs.nutritieProdus+'</div>';
                 if(produs.alergeniProdus!==''|| produs.alergeniProdus!=='-')
@@ -33,7 +33,7 @@ function incarcaMeniu()
                 $('.meniu').append('<div class="categorie" id="'+produs.categorieProdus+'">'+produs.categorieProdus+'</div>');
                 var cardProdus='<div class="cardProdus"><b>'+ produs.numeProdus + '</b> ('+produs.gramajProdus+') <p style="color:green;display:inline;position: absolute; right: 2rem;font-size:1rem;">'+produs.pretProdus+' lei</p>';
                 cardProdus+='<br/><div style="color:gray;">'+produs.descriereProdus+'</div>'
-                cardProdus+='<div style="font-size:12px;padding-left:1.2rem">'
+                cardProdus+='<div style="font-size:12px;padding-left:1.2rem">';
                 cardProdus+='<br/><p><b>Informații nutriționale 100g:</b></p><div style="color:gray;">'+ produs.nutritieProdus+'</div>';
                 if(produs.alergeniProdus!=='' || produs.alergeniProdus!=='-')
                 {
