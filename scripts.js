9734e12d496f46bac6b250d7c51b8b796f18cccf
baba-doner <135450542+baba-doner@users.noreply.github.com>
2023-06-22 18:30:14 +0300

    modificare adaugare ingrediente/valoare nutritionala

--- scripts.js
+++ scripts.js
@@ -15,8 +15,16 @@ function incarcaMeniu()
             if(categ===categCurenta)
             {
                 var cardProdus='<div class="cardProdus"><b>'+ produs.numeProdus + '</b> ('+produs.gramajProdus+') <p style="color:green;display:inline;position: absolute; right: 2rem;font-size:1rem;">'+produs.pretProdus+' lei</p>';
-                cardProdus+='<br><div style="color:gray;width:15rem;">'+produs.descriereProdus+'</div>'
-                cardProdus+='</div>';
+                cardProdus+='<br/><div style="color:gray;">'+produs.descriereProdus+'</div>'
+                cardProdus+='<div style="font-size:12px;padding:1.2rem">'
+                cardProdus+='<br/><p><b>Informații nutriționale 100g:</b></p><div style="color:gray;">'+ produs.nutritieProdus+'</div>';
+                if(produs.alergeniProdus!="" || produs.alergeniProdus!="-")
+                {
+                    cardProdus+='<p><b>Alergeni:</b></p><div style="color:gray;">'+ produs.alergeniProdus+'</div>';
+                }
+                
+
+                cardProdus+='</div></div>';
                 $('.meniu').append(cardProdus);
             }
             else
@@ -24,8 +32,14 @@ function incarcaMeniu()
                 categ=categCurenta;
                 $('.meniu').append('<div class="categorie" id="'+produs.categorieProdus+'">'+produs.categorieProdus+'</div>');
                 var cardProdus='<div class="cardProdus"><b>'+ produs.numeProdus + '</b> ('+produs.gramajProdus+') <p style="color:green;display:inline;position: absolute; right: 2rem;font-size:1rem;">'+produs.pretProdus+' lei</p>';
-                cardProdus+='<br><div style="color:gray;width:15rem;">'+produs.descriereProdus+'</div>'
-                cardProdus+='</div>';
+                cardProdus+='<br/><div style="color:gray;">'+produs.descriereProdus+'</div>'
+                cardProdus+='<div style="font-size:12px;padding-left:1.2rem">'
+                cardProdus+='<br/><p><b>Informații nutriționale 100g:</b></p><div style="color:gray;">'+ produs.nutritieProdus+'</div>';
+                if(produs.alergeniProdus!="" || produs.alergeniProdus!="-")
+                {
+                    cardProdus+='<p><b>Alergeni:</b></p><div style="color:gray;">'+ produs.alergeniProdus+'</div>';
+                }
+                cardProdus+='</div></div>';
                 $('.meniu').append(cardProdus);
             }
             
