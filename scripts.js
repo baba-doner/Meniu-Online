8ba268b7988ddba9ec8071d3278a4bfc8f12ca70
baba-doner <135450542+baba-doner@users.noreply.github.com>
2023-08-27 10:47:37 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,16 +11,21 @@ function incarcaMeniu()
         var categ="";
         var categCurenta="";
         $.each(data, function(index, produs) {
+            var descriereProdus;
+            if(produs.descriereProdus===undefined) 
+                descriereProdus="";
+            else
+            descriereProdus=produs.descriereProdus;
             categCurenta=produs.categorieProdus;
             if(categ===categCurenta)
             {
                 var cardProdus='<div class="cardProdus"><b>'+ produs.numeProdus + '</b> ('+produs.gramajProdus+') <p style="color:green;display:inline;position: absolute; right: 2rem;font-size:1rem;">'+produs.pretProdus+' lei</p>';
-                cardProdus+='<br/><div style="color:gray;">'+produs.descriereProdus+'</div>';
+                cardProdus+='<br/><div style="color:gray;">'+descriereProdus+'</div>';
                 cardProdus+='<div style="font-size:12px;padding:1.2rem">'
                 
                 if(produs.alergeniProdus!==''&& produs.alergeniProdus!=='-')
                 {
-cardProdus+='<br/><p><b>Informații nutriționale 100g:</b></p><div style="color:gray;">'+ produs.nutritieProdus+'</div>';
+                cardProdus+='<br/><p><b>Informații nutriționale 100g:</b></p><div style="color:gray;">'+ produs.nutritieProdus+'</div>';
                     cardProdus+='<p><b>Alergeni:</b></p><div style="color:gray;">'+ produs.alergeniProdus+'</div>';
                 }
                 
@@ -33,12 +38,12 @@ cardProdus+='<br/><p><b>Informații nutriționale 100g:</b></p><div style="color
                 categ=categCurenta;
                 $('.meniu').append('<div class="categorie" id="'+produs.categorieProdus+'">'+produs.categorieProdus+'</div>');
                 var cardProdus='<div class="cardProdus"><b>'+ produs.numeProdus + '</b> ('+produs.gramajProdus+') <p style="color:green;display:inline;position: absolute; right: 2rem;font-size:1rem;">'+produs.pretProdus+' lei</p>';
-                cardProdus+='<br/><div style="color:gray;">'+produs.descriereProdus+'</div>'
+                cardProdus+='<br/><div style="color:gray;">'+descriereProdus+'</div>'
                 cardProdus+='<div style="font-size:12px;padding-left:1.2rem">';
                 
                 if(produs.alergeniProdus!=='' && produs.alergeniProdus!=='-')
                 {
-cardProdus+='<br/><p><b>Informații nutriționale 100g:</b></p><div style="color:gray;">'+ produs.nutritieProdus+'</div>';
+                cardProdus+='<br/><p><b>Informații nutriționale 100g:</b></p><div style="color:gray;">'+ produs.nutritieProdus+'</div>';
                     cardProdus+='<p><b>Alergeni:</b></p><div style="color:gray;">'+ produs.alergeniProdus+'</div>';
                 }
                 cardProdus+='</div></div>';
